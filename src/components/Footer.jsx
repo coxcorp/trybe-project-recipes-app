2f07e5173a5e75623ae6aac9e10f4d88db737942
djsilva <coxcorp@gmail.com>
2021-12-08 16:54:41 -0300

    menu inferior

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealsIcon from '../images/mealIcon.svg';
+
+function Footer() {
+  return (
+    <footer data-testid="footer">
+      <Link to="/bebidas">
+        <img
+          src={ drinkIcon }
+          alt="DrinksButton"
+          data-testid="drinks-bottom-btn"
+        />
+      </Link>
+      <Link to="explorar">
+        <img
+          src={ exploreIcon }
+          alt="ExploreButton"
+          data-testid="explore-bottom-btn"
+        />
+      </Link>
+      <Link to="comidas">
+        <img
+          src={ mealsIcon }
+          alt="FoodButton"
+          data-testid="food-bottom-btn"
+        />
+      </Link>
+    </footer>
+  );
+}
+
+export default Footer;

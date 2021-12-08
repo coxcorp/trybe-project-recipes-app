eae94fd4bcfb7f67ad017fac80910a3d78256974
Effolex <Effolex@gmail.com>
2021-12-08 17:06:56 -0300

    Update storage.js

--- src/storage.js
+++ src/storage.js
@@ -5,23 +5,21 @@
 const saveTokensOnStorage = () => {
   localStorage.setItem('mealsToken', '1');
   localStorage.setItem('cocktailsToken', '1');
-}
+};
 
 /**
  * @name saveUserOnStorage
  * @description save the user email to the localStorage 'user' .
  * @param {string} email { id, type, area, category, alcoholicOrNot, name, image }
- * 
  */
 const saveUserOnStorage = (email) => {
   localStorage.setItem('user', { email });
-}
+};
 
 /**
  * @name addFavoriteRecipe
  * @description Add a favorite recipe to the localStorage 'favoriteRecipes' .
  * @param {object} recipe { id, type, area, category, alcoholicOrNot, name, image }
- * 
  */
 const addFavoriteRecipe = (recipe ) => {
   const storage = localStorage.getItem('favoriteRecipes');
@@ -32,13 +30,12 @@ const addFavoriteRecipe = (recipe ) => {
   const oldRecipes = JSON.parse(storage);
   localStorage.setItem('favoriteRecipes', JSON.stringify([...oldRecipes, recipe]));
 
-}
+};
 
 /**
  * @name editFavoriteRecipe
  * @description edit a favorite recipe to the localStorage 'favoriteRecipes' .
  * @param {object} recipe { id, type, area, category, alcoholicOrNot, name, image }
- * 
  */
 const editFavoriteRecipe = ( recipe ) => {
   const storage = localStorage.getItem('favoriteRecipes');
@@ -51,26 +48,24 @@ const editFavoriteRecipe = ( recipe ) => {
     return recipeMap;
   })
   localStorage.setItem('favoriteRecipes', JSON.stringify(newList));
-}
+};
 
 /**
  * @name removeFavoriteRecipe
  * @description remove a favorite recipe to the localStorage 'favoriteRecipes' .
  * @param {string} id { id, type, area, category, alcoholicOrNot, name, image }
- * 
  */
 const removeFavoriteRecipe = ( id ) => {
   const storage = localStorage.getItem('favoriteRecipes');
   const listOfRecipes = JSON.parse(storage);
   const newList = listOfRecipes.filter((recipeMap) => ( recipeMap.id !== id));
   localStorage.setItem('favoriteRecipes', JSON.stringify(newList));
-}
+};
 
 /**
  * @name addInProgressRecipe
  * @description add a recipe in progress to the localStorage 'inProgressRecipes' .
  * @param {object} recipe { id, type, area, category, alcoholicOrNot, name, image }
- * 
  */
 const addInProgressRecipe = (recipe) => {
   const storage = localStorage.getItem('inProgressRecipes');
@@ -80,13 +75,12 @@ const addInProgressRecipe = (recipe) => {
   }
   const oldRecipes = JSON.parse(storage);
   localStorage.setItem('inProgressRecipes', JSON.stringify([...oldRecipes, recipe]));
-}
+};
 
 /**
  * @name editInProgressRecipe
  * @description edit a recipe in progress to the localStorage 'inProgressRecipes' .
  * @param {object} recipe { id, type, area, category, alcoholicOrNot, name, image }
- * 
  */
 const editInProgressRecipe = ( recipe ) => {
   const storage = localStorage.getItem('inProgressRecipes');
@@ -99,29 +93,27 @@ const editInProgressRecipe = ( recipe ) => {
     return recipeMap;
   })
   localStorage.setItem('inProgressRecipes', JSON.stringify(newList));
-}
+};
 
 /**
  * @name removeProgressRecipe
  * @description remove a recipe in progress to the localStorage 'inProgressRecipes' .
  * @param {string} id { id, type, area, category, alcoholicOrNot, name, image }
- * 
  */
 const removeProgressRecipe = ( id ) => {
   const storage = localStorage.getItem('inProgressRecipes');
   const listOfRecipes = JSON.parse(storage);
   const newList = listOfRecipes.filter((recipeMap) => ( recipeMap.id !== id));
   localStorage.setItem('inProgressRecipes', JSON.stringify(newList));
-}
+};
 
 /**
  * @name cleanAllLocalStorage
  * @description clean the localStorage .
- * 
  */
 const cleanAllLocalStorage = () => {
   localStorage.clear();
-}
+};
 
 const storage = {
   addFavoriteRecipe,

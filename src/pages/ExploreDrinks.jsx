a6718244849a9929d6899791f5f74f05b1924ec8
Caio Lima <caiojlimah@gmail.com>
2021-12-08 19:31:32 -0300

    Requisitos 70 a 73

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 const ExploreDrinks = () => (
-  <Header />
+  <>
+    <Header />
+    <Link to="/explorar/bebidas/ingredientes">
+      <button type="button" data-testid="explore-by-ingredient">Por Ingredientes</button>
+    </Link>
+    <button type="button" data-testid="explore-surprise">Me Surpreenda!</button>
+  </>
 );
 
 export default ExploreDrinks;
--- src/pages/ExploreFood.jsx
+++ src/pages/ExploreFood.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 const ExploreFood = () => (
-  <Header />
+  <>
+    <Header />
+    <Link to="/explorar/comidas/ingredientes">
+      <button type="button" data-testid="explore-by-ingredient">Por Ingredientes</button>
+    </Link>
+    <Link to="/explorar/comidas/area">
+      <button type="button" data-testid="explore-by-area">Por Local de Origem</button>
+    </Link>
+    <button type="button" data-testid="explore-surprise">Me Surpreenda!</button>
+  </>
 );
 
 export default ExploreFood;

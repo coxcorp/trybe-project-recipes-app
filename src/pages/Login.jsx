cea5c33f1899557c83a18c1623f3de9e925a7c96
RuanCunha <ruanvcunha@hotmail.com>
2021-12-08 18:47:55 -0300

    Atualização login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Login = () => (
-  <h1>Aqui é o login</h1>
-);
+const Login = () => {
+  const [state, setState] = useState({ email: '', password: '', isButtonDisabled: true });
+
+  useEffect(() => {
+    const EMAIL_REGEX = /\S+@\S+\.\S+/.test(state.email);
+    console.log(EMAIL_REGEX);
+    const PASSWORD_LENGTH = 6;
+    if (EMAIL_REGEX && state.password.length >= PASSWORD_LENGTH) {
+      setState({ email: state.email, password: state.password, isButtonDisabled: false });
+      return;
+    }
+    setState({ email: state.email, password: state.password, isButtonDisabled: true });
+  }, [state.password, state.email]);
+
+  function handleChange({ target: { name, value } }) {
+    setState({ email: state.email, password: state.password, [name]: value });
+  }
+  return (
+    <div>
+      <input
+        data-testid="email-input"
+        type="email"
+        name="email"
+        onChange={ handleChange }
+        value={ state.email }
+      />
+      <input
+        data-testid="password-input"
+        type="password"
+        name="password"
+        onChange={ handleChange }
+        value={ state.password }
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ state.isButtonDisabled }
+      >
+        Entrar
+      </button>
+    </div>
+  );
+};
 
 export default Login;

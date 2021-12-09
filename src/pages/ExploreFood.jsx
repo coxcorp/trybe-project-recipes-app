83f3630cada2d80a9a5e698caee3be964a9fda44
djsilva <coxcorp@gmail.com>
2021-12-08 21:55:26 -0300

    add component footer

--- src/pages/ExploreFood.jsx
+++ src/pages/ExploreFood.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const ExploreFood = () => (
-  <Header />
+  <>
+    <Header />
+    <Footer />
+  </>
 );
 
 export default ExploreFood;
